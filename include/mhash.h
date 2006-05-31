f9745d36570419c161f528b0634c0470692c750d
Chris Hanson <cph@debian.org>
2006-05-31 14:55:40 -0400

    Imported Debian patch 0.9.6-3

--- include/mhash.h
+++ include/mhash.h
@@ -1,22 +1,11 @@
 #if !defined(__MHASH_H)
 #define __MHASH_H
 
-#ifdef VERSION
-#  define __MHASH_VERSION_KLUDGE VERSION
-#  undef VERSION
-#endif
-
 #include <mutils/mincludes.h>
 #include <mutils/mglobal.h>
 #include <mutils/mtypes.h>
 #include <mutils/mutils.h>
 #include <mutils/mhash.h>
-
-#define __MHASH_VERSION VERSION
-#undef VERSION
-#ifdef __MHASH_VERSION_KLUDGE
-#  define VERSION __MHASH_VERSION_KLUDGE
-#endif
 
 #endif
 
