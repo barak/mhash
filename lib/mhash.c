76717728bfa6f08c46969414360fe2c43c596665
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2013-06-24 09:57:50 +0100

    Imported Upstream version 0.8.14

---
+++ lib/mhash.c
@@ -0,0 +1,764 @@
+/*
+ *    Copyright (C) 1998 Nikos Mavroyanopoulos
+ *    Copyright (C) 1999,2000 Sascha Schumman, Nikos Mavroyanopoulos
+ *    Copyright (C) 2001 Nikos Mavroyanopoulos
+ *
+ *    This library is free software; you can redistribute it and/or modify it 
+ *    under the terms of the GNU Library General Public License as published 
+ *    by the Free Software Foundation; either version 2 of the License, or 
+ *    (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Library General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Library General Public
+ *    License along with this library; if not, write to the
+ *    Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *    Boston, MA 02111-1307, USA.
+ */
+
+
+/* $Id: mhash.c,v 1.28 2002/02/10 10:10:08 nmav Exp $ */
+
+#include <stdlib.h>
+
+#include "libdefs.h"
+
+#ifdef HAVE_PTHREAD_H
+#include <pthread.h>
+#endif
+
+#include "mhash_int.h"
+#include "mhash_crc32.h"
+#include "mhash_haval.h"
+#include "mhash_md5.h"
+#include "mhash_md4.h"
+#include "mhash_sha1.h"
+#include "mhash_tiger.h"
+#include "mhash_ripemd.h"
+#include "mhash_sha256.h"
+#include "mhash_adler32.h"
+#include "gosthash.h"
+
+/* 19/03/2000 Changes for better thread handling --nikos */
+
+#define MAX_BLOCK_SIZE 64
+
+#define MHASH_ENTRY(name, blksize, hash_pblock) \
+	{ #name, name, blksize, hash_pblock }
+
+struct mhash_hash_entry {
+	char *name;
+	hashid id;
+	size_t blocksize;
+	size_t hash_pblock;
+};
+
+static mhash_hash_entry algorithms[] = {
+	MHASH_ENTRY(MHASH_CRC32, 4, 0),
+	MHASH_ENTRY(MHASH_ADLER32, 4, 0),
+	MHASH_ENTRY(MHASH_MD5, 16, 64),
+	MHASH_ENTRY(MHASH_MD4, 16, 64),
+	MHASH_ENTRY(MHASH_SHA1, 20, 64),
+	MHASH_ENTRY(MHASH_SHA256, 32, 64),
+	MHASH_ENTRY(MHASH_HAVAL256, 32, 128),
+	MHASH_ENTRY(MHASH_HAVAL128, 16, 128),
+	MHASH_ENTRY(MHASH_HAVAL160, 20, 128),
+	MHASH_ENTRY(MHASH_HAVAL192, 24, 128),
+	MHASH_ENTRY(MHASH_HAVAL224, 28, 128),
+	MHASH_ENTRY(MHASH_RIPEMD160, 20, 64),
+	MHASH_ENTRY(MHASH_TIGER, 24, 64),
+	MHASH_ENTRY(MHASH_TIGER128, 16, 64),
+	MHASH_ENTRY(MHASH_TIGER160, 20, 64),
+	MHASH_ENTRY(MHASH_GOST, 32, 0),
+	MHASH_ENTRY(MHASH_CRC32B, 4, 0),
+	{0}
+};
+
+#define MHASH_LOOP(b) \
+	mhash_hash_entry *p; \
+	for(p = algorithms; p->name != NULL; p++) { b ; }
+
+#define MHASH_ALG_LOOP(a) \
+	MHASH_LOOP( if(p->id == type) { a; break; } )
+
+#ifndef MMAX
+# define MMAX(x,y) (x>y?x:y)
+#endif
+
+WIN32DLL_DEFINE size_t mhash_count(void)
+{
+	hashid count = 0;
+
+	MHASH_LOOP( count = MMAX( p->id, count) );
+
+	return count;
+}
+
+WIN32DLL_DEFINE size_t mhash_get_block_size(hashid type)
+{
+	size_t ret = 0;
+
+	MHASH_ALG_LOOP(ret = p->blocksize);
+	return ret;
+}
+
+/* function created in order for mhash to compile under WIN32 */
+char *mystrdup(char *str)
+{
+	char *ret;
+	if (str==NULL) return NULL;
+	
+	if ( (ret = malloc(strlen(str) + 1)) == NULL) return NULL;
+	strcpy(ret, str);
+
+	return ret;
+
+}
+
+WIN32DLL_DEFINE hashid mhash_get_mhash_algo( MHASH tmp) {
+	return tmp->algorithm_given;
+}
+
+WIN32DLL_DEFINE char *mhash_get_hash_name(hashid type)
+{
+	char *ret = NULL;
+
+	/* avoid prefix */
+	MHASH_ALG_LOOP( ret = p->name);
+
+	if (ret!=NULL) ret += sizeof("MHASH_") - 1;
+	
+	return mystrdup(ret);
+}
+
+WIN32DLL_DEFINE const char *mhash_get_hash_name_static(hashid type)
+{
+	char *ret = NULL;
+
+	/* avoid prefix */
+	MHASH_ALG_LOOP( ret = p->name);
+	
+	if ( ret !=NULL) ret+= sizeof("MHASH_") - 1;
+
+	return ret;
+}
+
+MHASH mhash_cp(MHASH from) {
+MHASH ret;
+
+	if ( (ret = malloc(sizeof(MHASH_INSTANCE))) == NULL) return MHASH_FAILED;
+	memcpy(ret, from, sizeof(MHASH_INSTANCE));
+	
+	/* copy the internal state also */
+	if ( (ret->state=malloc(ret->state_size)) == NULL) return MHASH_FAILED;
+	memcpy( ret->state, from->state, ret->state_size);
+	
+	/* copy the key in case of hmac*/
+	if (ret->hmac_key_size!=0) {
+		if ((ret->hmac_key=malloc(ret->hmac_key_size)) == NULL) return MHASH_FAILED;
+		memcpy(ret->hmac_key, from->hmac_key, ret->hmac_key_size);
+	}
+	return ret;
+
+}
+MHASH mhash_init_int(const hashid type)
+{
+	MHASH ret;
+
+	if ( (ret = malloc( sizeof(MHASH_INSTANCE))) == NULL) return MHASH_FAILED;
+	ret->algorithm_given = type;
+	ret->hmac_key = NULL;
+	ret->state = NULL;
+	ret->hmac_key_size = 0;
+	
+	switch (type) {
+	case MHASH_CRC32:
+	case MHASH_CRC32B:
+		ret->state_size = sizeof(word32);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		mhash_clear_crc32((void *) ret->state);
+		break;
+	case MHASH_ADLER32:
+		ret->state_size = sizeof(word32);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		mhash_clear_adler32((void *) ret->state);
+		break;
+	case MHASH_MD5:
+		ret->state_size = sizeof(MD5_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		MD5Init((void *) ret->state);
+		break;
+	case MHASH_MD4:
+		ret->state_size = sizeof(MD4_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		MD4Init((void *) ret->state);
+		break;
+	case MHASH_SHA1:
+		ret->state_size = sizeof(SHA_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		sha_init((void *) ret->state);
+		break;
+	case MHASH_SHA256:
+		ret->state_size = sizeof( SHA256_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		sha256_init((void *) ret->state);
+		break;
+	case MHASH_HAVAL256:
+		ret->state_size = sizeof(havalContext);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		havalInit((void *) ret->state, 3, 256);
+		break;
+	case MHASH_HAVAL224:
+		ret->state_size = sizeof(havalContext);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		havalInit((void *) ret->state, 3, 224);
+		break;
+	case MHASH_HAVAL192:
+		ret->state_size = sizeof(havalContext);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		havalInit((void *) ret->state, 3, 192);
+		break;
+	case MHASH_HAVAL160:
+		ret->state_size = sizeof(havalContext);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		havalInit((void *) ret->state, 3, 160);
+		break;
+	case MHASH_HAVAL128:
+		ret->state_size = sizeof(havalContext);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		havalInit((void *) ret->state, 3, 128);
+		break;
+	case MHASH_RIPEMD160:
+		ret->state_size = sizeof(RIPEMD_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		ripemd_init((void *) ret->state);
+		break;
+	case MHASH_TIGER:
+	case MHASH_TIGER128:
+	case MHASH_TIGER160:
+		ret->state_size = sizeof(TIGER_CTX);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		tiger_init((void*) ret->state);
+		break;
+	case MHASH_GOST:
+		ret->state_size = sizeof(GostHashCtx);
+		if ( (ret->state = malloc(ret->state_size)) == NULL) {
+			free(ret);
+			return MHASH_FAILED;
+		}
+		gosthash_reset((void *) ret->state);
+		break;
+	default:
+		free(ret);
+		ret = MHASH_FAILED;
+		break;
+	}
+
+	return ret;
+}
+
+#define MIX32(a) \
+	(((unsigned long)((unsigned char *)(a))[0]) | \
+	(((unsigned long)((unsigned char *)(a))[1]) << 8)| \
+	(((unsigned long)((unsigned char *)(a))[2]) << 16)| \
+	(((unsigned long)((unsigned char *)(a))[3]) << 24))
+
+
+#ifdef WORDS_BIGENDIAN
+void mhash_32bit_conversion(word32 * ptr, size_t count)
+{
+	size_t i;
+
+	for (i = 0; i < count; i++) {
+		ptr[i] = MIX32(&ptr[i]);
+	}
+}
+#else
+#define mhash_32bit_conversion(a,b)
+#endif
+
+/* plaintext should be a multiply of the algorithm's block size */
+
+int mhash(MHASH thread, const void *plaintext, size_t size)
+{
+
+	switch (thread->algorithm_given) {
+	case MHASH_CRC32:
+		mhash_crc32((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_ADLER32:
+		mhash_adler32((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_CRC32B:
+		crc32b((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_MD5:
+		MD5Update((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_MD4:
+		MD4Update((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_SHA1:
+		sha_update((void *) thread->state, (void *) plaintext,
+			   size);
+		break;
+	case MHASH_SHA256:
+		sha256_update((void *) thread->state, (void *) plaintext, size);
+		break;
+	case MHASH_HAVAL256:
+	case MHASH_HAVAL224:
+	case MHASH_HAVAL192:
+	case MHASH_HAVAL160:
+	case MHASH_HAVAL128:
+		havalUpdate((void *) thread->state, plaintext, size);
+		break;
+	case MHASH_RIPEMD160:
+		ripemd_update((void *) thread->state, (void *) plaintext,
+			      size);
+		break;
+	case MHASH_TIGER:
+	case MHASH_TIGER160:
+	case MHASH_TIGER128:
+		tiger_update((void*)thread->state, (void*)plaintext, size);
+		break;
+	case MHASH_GOST:
+		gosthash_update((void *) thread->state, plaintext, size);
+		break;
+	}
+
+	return 0;
+}
+
+
+WIN32DLL_DEFINE
+    void mhash_deinit(MHASH thread, void *result)
+{
+
+	if (result!=NULL)
+	switch (thread->algorithm_given) {
+	case MHASH_CRC32:
+	case MHASH_CRC32B:
+		mhash_get_crc32(result, (void *) thread->state);
+		break;
+	case MHASH_ADLER32:
+		mhash_get_adler32(result, (void *) thread->state);
+		break;
+	case MHASH_MD5:
+		MD5Final( result, (void *) thread->state);
+		break;
+	case MHASH_MD4:
+		MD4Final( result, (void *) thread->state);
+		break;
+	case MHASH_SHA1:
+		sha_final((void *) thread->state);
+		sha_digest((void *) thread->state, result);
+		break;
+	case MHASH_SHA256:
+		sha256_final((void *) thread->state);
+		sha256_digest((void *) thread->state, result);
+		break;
+	case MHASH_HAVAL256:
+	case MHASH_HAVAL224:
+	case MHASH_HAVAL192:
+	case MHASH_HAVAL160:
+	case MHASH_HAVAL128:
+		havalFinal((void *) thread->state, result);
+		break;
+	case MHASH_RIPEMD160:
+		ripemd_final((void *) thread->state);
+		ripemd_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER:
+		tiger_final((void*) thread->state);
+		tiger_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER128:
+		tiger_final((void*) thread->state);
+		tiger128_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER160:
+		tiger_final((void*) thread->state);
+		tiger160_digest((void *) thread->state, result);
+		break;
+	case MHASH_GOST:
+		gosthash_final((void *) thread->state, result);
+		break;
+	}
+
+	if (NULL != thread->state) {
+		free(thread->state);
+	}
+	free(thread);
+
+	return;
+}
+
+WIN32DLL_DEFINE
+    void *mhash_end_m(MHASH thread, void *(*hash_malloc) (size_t))
+{
+	void *digest;
+	int size;
+
+	size = mhash_get_block_size( thread->algorithm_given);
+	
+	digest = hash_malloc( size);
+	if (digest==NULL) return NULL;
+	
+	mhash_deinit( thread, digest);
+	
+	return digest;
+}
+
+WIN32DLL_DEFINE void *mhash_end(MHASH thread)
+{
+	return mhash_end_m(thread, malloc);
+}
+
+
+WIN32DLL_DEFINE MHASH mhash_init(const hashid type)
+{
+	MHASH ret = MHASH_FAILED;
+
+	ret = mhash_init_int(type);
+
+	return ret;
+}
+
+/* HMAC functions */
+
+WIN32DLL_DEFINE size_t mhash_get_hash_pblock(hashid type)
+{
+	size_t ret = 0;
+
+	MHASH_ALG_LOOP(ret = p->hash_pblock);
+	return ret;
+}
+
+WIN32DLL_DEFINE
+    int mhash_hmac_deinit(MHASH thread, void *result)
+{
+	unsigned char *opad;
+	unsigned char _opad[MAX_BLOCK_SIZE];
+	MHASH tmptd;
+	int i, opad_alloc = 0;
+
+	if (thread->hmac_block > MAX_BLOCK_SIZE) {
+		opad = malloc(thread->hmac_block);
+		if (opad == NULL) return -1;
+		opad_alloc = 1;
+	} else {
+		opad = _opad;
+	}
+
+
+	for (i = 0; i < thread->hmac_key_size; i++) {
+		opad[i] = (0x5C) ^ thread->hmac_key[i];
+	}
+	for (; i < thread->hmac_block; i++) {
+		opad[i] = (0x5C);
+	}
+
+	tmptd = mhash_init(thread->algorithm_given);
+	mhash(tmptd, opad, thread->hmac_block);
+
+	switch (thread->algorithm_given) {
+	case MHASH_CRC32:
+	case MHASH_CRC32B:
+		mhash_get_crc32( result, (void *) thread->state);
+		break;
+	case MHASH_ADLER32:
+		mhash_get_adler32( result, (void *) thread->state);
+		break;
+	case MHASH_MD5:
+		MD5Final( result, (void *) thread->state);
+		break;
+	case MHASH_MD4:
+		MD4Final( result, (void *) thread->state);
+		break;
+	case MHASH_SHA1:
+		sha_final((void *) thread->state);
+		sha_digest((void *) thread->state, result);
+		break;
+	case MHASH_SHA256:
+		sha256_final((void *) thread->state);
+		sha256_digest((void *) thread->state, result);
+		break;
+	case MHASH_HAVAL256:
+	case MHASH_HAVAL224:
+	case MHASH_HAVAL192:
+	case MHASH_HAVAL160:
+	case MHASH_HAVAL128:
+		havalFinal((void *) thread->state, result);
+		break;
+	case MHASH_RIPEMD160:
+		ripemd_final((void *) thread->state);
+		ripemd_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER:
+		tiger_final((void *) thread->state);
+		tiger_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER128:
+		tiger_final((void *) thread->state);
+		tiger128_digest((void *) thread->state, result);
+		break;
+	case MHASH_TIGER160:
+		tiger_final((void *) thread->state);
+		tiger160_digest((void *) thread->state, result);
+		break;
+	case MHASH_GOST:
+		gosthash_final((void *) thread->state, result);
+		break;
+	}
+
+	if (result!=NULL)
+		mhash(tmptd, result,
+		      mhash_get_block_size(thread->algorithm_given));
+
+	free(thread->state);
+
+	if (opad_alloc!=0) free(opad);
+	
+	mhash_bzero(thread->hmac_key, thread->hmac_key_size);
+	free(thread->hmac_key);
+	free(thread);
+
+	mhash_deinit(tmptd, result);
+
+	return 0;
+}
+
+
+WIN32DLL_DEFINE
+    void *mhash_hmac_end_m(MHASH thread, void *(*hash_malloc) (size_t))
+{
+	void *digest;
+
+	digest =
+	    hash_malloc(mhash_get_block_size
+			(thread->algorithm_given));
+	if (digest == NULL) return NULL;
+
+	mhash_hmac_deinit( thread, digest);
+	
+	return digest;
+	
+}
+
+WIN32DLL_DEFINE void *mhash_hmac_end(MHASH thread)
+{
+	return mhash_hmac_end_m(thread, malloc);
+}
+
+
+WIN32DLL_DEFINE
+    MHASH mhash_hmac_init(const hashid type, void *key, int keysize,
+			  int block)
+{
+	MHASH ret = MHASH_FAILED;
+	MHASH tmptd;
+	unsigned char *ipad;
+	unsigned char _ipad[MAX_BLOCK_SIZE];
+	int i, ipad_alloc=0;
+
+	if (block == 0) {
+		block = 64;	/* the default for ripemd,md5,sha-1 */
+	}
+
+	ret = mhash_init_int(type);
+
+	if (ret != MHASH_FAILED) {
+		/* Initial hmac calculations */
+		ret->hmac_block = block;
+
+		if ( ret->hmac_block > MAX_BLOCK_SIZE) {
+			ipad = malloc(ret->hmac_block);
+			if (ipad == NULL) return MHASH_FAILED;
+			ipad_alloc = 1;
+		} else {
+			ipad = _ipad;
+		}
+		
+		if (keysize > ret->hmac_block) {
+			tmptd = mhash_init(type);
+			mhash(tmptd, key, keysize);
+			ret->hmac_key_size = mhash_get_block_size(type);
+			ret->hmac_key = mhash_end(tmptd);
+		} else {
+			ret->hmac_key = calloc(1, ret->hmac_block);
+			memcpy(ret->hmac_key, key, keysize);
+			ret->hmac_key_size = ret->hmac_block;
+		}
+
+		/* IPAD */
+
+		for (i = 0; i < ret->hmac_key_size; i++) {
+			ipad[i] = (0x36) ^ ret->hmac_key[i];
+		}
+		for (; i < ret->hmac_block; i++) {
+			ipad[i] = (0x36);
+		}
+
+		mhash(ret, ipad, ret->hmac_block);
+
+		if (ipad_alloc!=0) free(ipad);
+	}
+
+
+	return ret;
+}
+
+WIN32DLL_DEFINE void mhash_free(void *ptr)
+{
+	free(ptr);
+}
+
+/*
+  Saves the state of a hashing algorithm such that it can be
+  restored at some later point in time using
+  mhash_restore_state().
+  
+  mem_size should contain the size of the given _mem pointer.
+  Otherwise the required size will be copied there.
+
+  Original version and idea by Blake Stephen <Stephen.Blake@veritect.com>
+*/
+WIN32DLL_DEFINE int mhash_save_state_mem(MHASH thread, void *_mem, int* mem_size )
+{
+	int tot_size, pos;
+	unsigned char* mem = _mem;
+	
+	tot_size = sizeof(thread->algorithm_given) + sizeof(thread->hmac_key_size)
+		+ sizeof(thread->hmac_block) + thread->hmac_key_size +
+		+ sizeof(thread->state_size) + thread->state_size;
+	
+	if ( *mem_size < tot_size) {
+		*mem_size = tot_size;
+		return -1;
+	}
+	
+	if ( mem != NULL) {
+		pos = 0;
+		memcpy( mem, &thread->algorithm_given, sizeof(thread->algorithm_given));
+		pos = sizeof( thread->algorithm_given);
+		
+		memcpy( &mem[pos], &thread->hmac_key_size, sizeof(thread->hmac_key_size));
+		pos += sizeof(thread->hmac_key_size);
+
+		memcpy( &mem[pos], &thread->hmac_block, sizeof(thread->hmac_block));
+		pos += sizeof(thread->hmac_block);
+
+		memcpy( &mem[pos], thread->hmac_key, thread->hmac_key_size);
+		pos += thread->hmac_key_size;
+
+		memcpy( &mem[pos], &thread->state_size, sizeof(thread->state_size));
+		pos += sizeof(thread->state_size);
+
+		memcpy( &mem[pos], thread->state, thread->state_size);
+		pos += thread->state_size;
+
+	}
+	return 0;
+}
+
+
+/*
+  Restores the state of a hashing algorithm that was saved
+  using mhash_save_state(). Use like mhash_init.
+*/
+WIN32DLL_DEFINE MHASH mhash_restore_state_mem(void* _mem)
+{
+	unsigned char* mem = _mem;
+	hashid algorithm_given;
+	MHASH ret = MHASH_FAILED;
+	int pos;
+
+	if (mem==NULL)
+		return ret;
+
+	memcpy( &algorithm_given, mem, sizeof(algorithm_given));
+	
+	if ((ret = mhash_init(algorithm_given)) == MHASH_FAILED) {
+		return ret;
+	}
+
+	ret->algorithm_given = algorithm_given;
+	
+	pos = sizeof(algorithm_given);
+
+ 	memcpy( &ret->hmac_key_size, &mem[pos], sizeof(ret->hmac_key_size));
+	pos += sizeof( ret->hmac_key_size);
+		
+	memcpy( &ret->hmac_block, &mem[pos], sizeof(ret->hmac_block));
+	pos += sizeof(ret->hmac_block);
+
+	if (ret->hmac_key_size != 0) {
+		ret->hmac_key = malloc( ret->hmac_key_size);
+		if (ret->hmac_key==NULL) goto freeall;
+	
+		memcpy( ret->hmac_key, &mem[pos], ret->hmac_key_size);
+		pos += sizeof(ret->hmac_key_size);
+	}
+
+	memcpy( &ret->state_size, &mem[pos], sizeof(ret->state_size));
+	pos += sizeof( ret->state_size);
+
+	ret->state = malloc( ret->state_size);
+	if (ret->state==NULL) goto freeall;
+
+	memcpy( ret->state, &mem[pos], ret->state_size);
+	pos += ret->state_size;
+
+	return ret;
+
+	freeall:
+		/* This uses too much internals
+		 */
+		free( ret->state);
+		free( ret->hmac_key);
+		free( ret);
+		return MHASH_FAILED;
+}
+
---
+++ src/hash_test.sh
@@ -0,0 +1,170 @@
+#! /bin/sh
+
+# $Id: hash_test.sh,v 1.10 2001/10/27 09:42:22 nmav Exp $
+
+if (echo "testing\c"; echo 1,2,3) | grep c >/dev/null; then
+  if (echo $ac_n testing; echo 1,2,3) | sed s/-n/xn/ | grep xn >/dev/null; then
+    ac_n= ac_c='
+' ac_t='    '
+  else
+    ac_n=-n ac_c= ac_t=
+  fi
+else
+  ac_n= ac_c='\c' ac_t=
+fi
+
+test_hash ( ) {
+	if test "$#" != "3" ; then
+		echo "usage: test_hash id plain expected"
+		exit 1
+	fi
+	
+	plainlen=`echo $ac_n "$2$ac_c" | wc -c`
+	
+	got=`echo $ac_n "$2$ac_c" | ./driver $1 $plainlen | tr "[:lower:]" "[:upper:]"`
+
+	want=`echo $ac_n "$3$ac_c" | tr "[:lower:]" "[:upper:]"`
+	
+	if test "$got" = ""; then
+		echo "This algorithm($1) might not available"
+	else
+		if test "$got" != "$want" ; then
+			echo "  -- TEST FAILED: $1 \"$2\""
+			echo "got \"$got\""
+			echo "but expected \"$want\""
+			exit 1
+		else
+			echo $ac_n ".$ac_c"
+		fi
+	fi
+
+}
+
+echo $ac_n "testing CRC32 $ac_c"
+test_hash 0 "checksum" 7FBEB02E
+echo ""
+
+echo $ac_n "testing CRC32B $ac_c"
+test_hash 9 "checksum" 9ADF6FDE
+echo ""
+
+echo $ac_n "testing MD5 $ac_c"
+test_hash 1 "" D41D8CD98F00B204E9800998ECF8427E
+test_hash 1 a 0CC175B9C0F1B6A831C399E269772661
+test_hash 1 abc 900150983CD24FB0D6963F7D28E17F72
+test_hash 1 "message digest" F96B697D7CB7938D525A2F31AAF161D0
+test_hash 1 abcdefghijklmnopqrstuvwxyz C3FCD3D76192E4007DFB496CCA67E13B
+test_hash 1 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \
+		D174AB98D277D9F5A5611C2C9F419D9F
+test_hash 1 12345678901234567890123456789012345678901234567890123456789012345678901234567890 \
+		57EDF4A22BE3C955AC49DA2E2107B67A
+echo ""
+
+echo $ac_n "testing SHA1 $ac_c"
+test_hash 2 abc A9993E364706816ABA3E25717850C26C9CD0D89D
+test_hash 2 abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq \
+		84983E441C3BD26EBAAE4AA1F95129E5E54670F1
+echo ""
+
+echo $ac_n "testing HAVAL256 $ac_c"
+test_hash 3 abcdefghijklmnopqrstuvwxyz \
+		72FAD4BDE1DA8C8332FB60561A780E7F504F21547B98686824FC33FC796AFA76
+test_hash 3 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \
+		899397D96489281E9E76D5E65ABAB751F312E06C06C07C9C1D42ABD31BB6A404
+echo 
+
+echo $ac_n "testing HAVAL224 $ac_c"
+test_hash 10 "0123456789" \
+		EE345C97A58190BF0F38BF7CE890231AA5FCF9862BF8E7BEBBF76789
+echo 
+
+echo $ac_n "testing HAVAL192 $ac_c"
+test_hash 11 "HAVAL" \
+		8DA26DDAB4317B392B22B638998FE65B0FBE4610D345CF89
+echo 
+
+echo $ac_n "testing HAVAL160 $ac_c"
+test_hash 12 "a" \
+		4DA08F514A7275DBC4CECE4A347385983983A830
+echo 
+
+echo $ac_n "testing HAVAL128 $ac_c"
+test_hash 13 "" \
+		C68F39913F901F3DDF44C707357A7D70
+echo 
+
+
+echo $ac_n "testing RIPEMD160 $ac_c"
+test_hash 5 "" 9C1185A5C5E9FC54612808977EE8F548B2258D31
+test_hash 5 a 0BDC9D2D256B3EE9DAAE347BE6F4DC835A467FFE
+test_hash 5 abc 8EB208F7E05D987A9B044A8E98C6B087F15A0BFC
+test_hash 5 "message digest" 5D0689EF49D2FAE572B881B123A85FFA21595F36
+test_hash 5 abcdefghijklmnopqrstuvwxyz F71C27109C692C1B56BBDCEB5B9D2865B3708DBC
+test_hash 5 abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq \
+		12A053384A9C0C88E405A06C27DCF49ADA62EB2B
+test_hash 5 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \
+		B0E20B6E3116640286ED3A87A5713079B21F5189
+echo ""
+
+
+echo $ac_n "testing TIGER $ac_c"
+test_hash 7 "" 24F0130C63AC933216166E76B1BB925FF373DE2D49584E7A
+test_hash 7 abc F258C1E88414AB2A527AB541FFC5B8BF935F7B951C132951
+test_hash 7 Tiger 9F00F599072300DD276ABB38C8EB6DEC37790C116F9D2BDF
+test_hash 7 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+- \
+		 87FB2A9083851CF7470D2CF810E6DF9EB586445034A5A386
+test_hash 7 "ABCDEFGHIJKLMNOPQRSTUVWXYZ=abcdefghijklmnopqrstuvwxyz+0123456789" \
+		467DB80863EBCE488DF1CD1261655DE957896565975F9197
+test_hash 7 "Tiger - A Fast New Hash Function, by Ross Anderson and Eli Biham" \
+		 0C410A042968868A1671DA5A3FD29A725EC1E457D3CDB303
+test_hash 7 "Tiger - A Fast New Hash Function, by Ross Anderson and Eli Biham, proceedings of Fast Software Encryption 3, Cambridge." \
+		 EBF591D5AFA655CE7F22894FF87F54AC89C811B6B0DA3193
+test_hash 7 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-" \
+		 00B83EB4E53440C576AC6AAEE0A7485825FD15E70A59FFE4
+echo ""
+
+echo $ac_n "testing TIGER160 $ac_c"
+test_hash 15 "" 24F0130C63AC933216166E76B1BB925FF373DE2D
+test_hash 15 abc F258C1E88414AB2A527AB541FFC5B8BF935F7B95
+test_hash 15 Tiger 9F00F599072300DD276ABB38C8EB6DEC37790C11
+test_hash 15 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+- \
+		 87FB2A9083851CF7470D2CF810E6DF9EB5864450
+test_hash 15 "ABCDEFGHIJKLMNOPQRSTUVWXYZ=abcdefghijklmnopqrstuvwxyz+0123456789" \
+		467DB80863EBCE488DF1CD1261655DE957896565
+echo ""
+
+echo $ac_n "testing TIGER128 $ac_c"
+test_hash 14 "" 24F0130C63AC933216166E76B1BB925F
+test_hash 14 abc F258C1E88414AB2A527AB541FFC5B8BF
+test_hash 14 Tiger 9F00F599072300DD276ABB38C8EB6DEC
+echo ""
+
+
+
+echo $ac_n "testing GOST $ac_c"
+test_hash 8 "This is message, length=32 bytes" \
+		B1C466D37519B82E8319819FF32595E047A28CB6F83EFF1C6916A815A637FFFA
+test_hash 8 "Suppose the original message has length = 50 bytes" \
+		471ABA57A60A770D3A76130635C1FBEA4EF14DE51F78B4AE57DD893B62F55208
+echo ""
+
+echo $ac_n "testing MD4 $ac_c"
+test_hash 16 "" 31D6CFE0D16AE931B73C59D7E0C089C0
+test_hash 16 a BDE52CB31DE33E46245E05FBDBD6FB24
+test_hash 16 abc A448017AAF21D8525FC10AE87AA6729D
+test_hash 16 "message digest" D9130A8164549FE818874806E1C7014B
+test_hash 16 abcdefghijklmnopqrstuvwxyz D79E1C308AA5BBCDEEA8ED63DF412DA9
+test_hash 16 12345678901234567890123456789012345678901234567890123456789012345678901234567890 \
+		E33B4DDC9C38F2199C3E7B164FCC0536
+echo ""
+
+echo $ac_n "testing SHA-256 $ac_c"
+test_hash 17 abc ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+test_hash 17 abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq 248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1
+test_hash 17 abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1
+echo ""
+
+
+echo "everything seems to be fine :-)"
+
+exit 0
